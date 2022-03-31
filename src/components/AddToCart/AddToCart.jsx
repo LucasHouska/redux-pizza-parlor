2bd95461164b1885b00d899c597871b1d24a10b2
Lucas Houska <ljhouska75@gmail.com>
2022-03-31 14:13:14 -0500

    add next button functionality

--- src/components/AddToCart/AddToCart.jsx
+++ src/components/AddToCart/AddToCart.jsx
@@ -1,14 +1,24 @@
 import PizzaList from '../PizzaList/PizzaList';
+import {Link} from 'react-router-dom'
+import { useHistory } from 'react-router-dom';
 
 
 function AddToCart() {
+
+    const history = useHistory()
+
+
+    const handleNext = () => {
+        history.push('/customerInfo')
+    }
+
     return (
         <>
             <header className='App-header'>
                 <h1 className='App-title'>Prime Pizza</h1>
             </header>
             <PizzaList />
-            <button>NEXT</button>
+            <button onClick={handleNext}>NEXT</button>
         </>
     )
 }
